48b85a9c65f708bc783e61661894540d238db88e
Eugenio Costa <tutecosta@gmail.com>
2009-07-07 00:48:19 -0300

    White-space trimming.

--- thumbnail.php
+++ thumbnail.php
@@ -79,7 +79,7 @@ class ThumbnailComponent extends Object
 			}
 
 			// Image uploaded, return the file name
-			return $filename;   
+			return $filename;
 		}
 	}
 
@@ -123,129 +123,117 @@ class ThumbnailComponent extends Object
 	function resizeImage($cType = 'resize', $srcfolder, $srcname, $dstfolder, $dstname = false, $newWidth=false, $newHeight=false, $quality = 75)
 	{
 		$srcimg = $srcfolder.DS.$srcname;
-		list($oldWidth, $oldHeight, $type) = getimagesize($srcimg); 
+		list($oldWidth, $oldHeight, $type) = getimagesize($srcimg);
 		$ext = $this->image_type_to_extension($type);
 
 		//check to make sure that the file is writeable, if so, create destination image (temp image)
-		if (is_writeable($dstfolder))
-		{
+		if (is_writeable($dstfolder)) {
 			$dstimg = $dstfolder.DS.$dstname;
-		}
-		else
-		{
-			//if not let developer know
+		} else {
+			// if dirFolder not writeable, let developer know
 			debug("You must allow proper permissions for image processing. And the folder has to be writable.");
 			debug("Run \"chmod 777 on '$dstfolder' folder\"");
 			exit();
 		}
 
-		//check to make sure that something is requested, otherwise there is nothing to resize.
-		//although, could create option for quality only
-		if ($newWidth OR $newHeight)
-		{
-			/*
-			 * check to make sure temp file doesn't exist from a mistake or system hang up.
-			 * If so delete.
-			 */
-			if(file_exists($dstimg))
-			{
+		// Check if something is requested, otherwise do not resize
+		if ($newWidth or $newHeight) {
+			/* If temp file exists, delete it */
+			if(file_exists($dstimg)) {
 				unlink($dstimg);
-			}
-			else
-			{
-				switch ($cType){
-					default:
-					case 'resize':
-						# Maintains the aspect ration of the image and makes sure that it fits
-						# within the maxW(newWidth) and maxH(newHeight) (thus some side will be smaller)
-						$widthScale = 2;
-						$heightScale = 2;
+			} else {
+				switch ($cType) {
+				default:
+				case 'resize':
+					// Maintains the aspect ratio of the image and makes sure that it fits
+					// within the maxW(newWidth) and maxH(newHeight) (thus some side will be smaller)
+					$widthScale = 2;
+					$heightScale = 2;
 
-						// Check to see that we are not over resizing, otherwise, set the new scale
-						if($newWidth) {
-							if($newWidth > $oldWidth) $newWidth = $oldWidth;
-							$widthScale = 	$newWidth / $oldWidth;
-						}
-						if($newHeight) {
-							if($newHeight > $oldHeight) $newHeight = $oldHeight;
-							$heightScale = $newHeight / $oldHeight;
-						}
-						//debug("W: $widthScale  H: $heightScale<br>");
-						if($widthScale < $heightScale) {
-							$maxWidth = $newWidth;
-							$maxHeight = false;							
-						} elseif ($widthScale > $heightScale ) {
-							$maxHeight = $newHeight;
-							$maxWidth = false;
-						} else {
-							$maxHeight = $newHeight;
-							$maxWidth = $newWidth;
-						}
-
-						if($maxWidth > $maxHeight){
-							$applyWidth = $maxWidth;
-							$applyHeight = ($oldHeight*$applyWidth)/$oldWidth;
-						} elseif ($maxHeight > $maxWidth) {
-							$applyHeight = $maxHeight;
-							$applyWidth = ($applyHeight*$oldWidth)/$oldHeight;
-						} else {
-							$applyWidth = $maxWidth; 
-								$applyHeight = $maxHeight;
-						}
-						$startX = 0;
-						$startY = 0;
-						break;
-					case 'resizeCrop':
-
-						// Check to see that we are not over resizing, otherwise, set the new scale						
-						// -- resize to max, then crop to center
-						if($newWidth > $oldWidth) $newWidth = $oldWidth;	
-							$ratioX = $newWidth / $oldWidth;
-
+					// Check to see that we are not over resizing, otherwise, set the new scale
+					if($newWidth) {
+						if($newWidth > $oldWidth) $newWidth = $oldWidth;
+						$widthScale = 	$newWidth / $oldWidth;
+					}
+					if($newHeight) {
 						if($newHeight > $oldHeight) $newHeight = $oldHeight;
-							$ratioY = $newHeight / $oldHeight;									
+						$heightScale = $newHeight / $oldHeight;
+					}
+					if($widthScale < $heightScale) {
+						$maxWidth = $newWidth;
+						$maxHeight = false;
+					} elseif ($widthScale > $heightScale ) {
+						$maxHeight = $newHeight;
+						$maxWidth = false;
+					} else {
+						$maxHeight = $newHeight;
+						$maxWidth = $newWidth;
+					}
 
-						if ($ratioX < $ratioY) { 
-							$startX = round(($oldWidth - ($newWidth / $ratioY))/2);
-							$startY = 0;
-							$oldWidth = round($newWidth / $ratioY);
-							$oldHeight = $oldHeight;
-						} else { 
-							$startX = 0;
-							$startY = round(($oldHeight - ($newHeight / $ratioX))/2);
-							$oldWidth = $oldWidth;
-							$oldHeight = round($newHeight / $ratioX);
-						}
-						$applyWidth = $newWidth;
-						$applyHeight = $newHeight;
-						break;
-					case 'crop':
-						// -- a straight centered crop
-						$startY = ($oldHeight - $newHeight)/2;
-						$startX = ($oldWidth - $newWidth)/2;
-						$oldHeight = $newHeight;
-						$applyHeight = $newHeight;
-						$oldWidth = $newWidth; 
-						$applyWidth = $newWidth;
-						break;
+					if($maxWidth > $maxHeight){
+						$applyWidth = $maxWidth;
+						$applyHeight = ($oldHeight*$applyWidth)/$oldWidth;
+					} elseif ($maxHeight > $maxWidth) {
+						$applyHeight = $maxHeight;
+						$applyWidth = ($applyHeight*$oldWidth)/$oldHeight;
+					} else {
+						$applyWidth = $maxWidth;
+						$applyHeight = $maxHeight;
+					}
+					$startX = 0;
+					$startY = 0;
+					break;
+
+				case 'resizeCrop':
+					// Check to see that we are not over resizing, otherwise, set the new scale
+					// -- resize to max, then crop to center
+					if($newWidth > $oldWidth) $newWidth = $oldWidth;
+						$ratioX = $newWidth / $oldWidth;
+
+					if($newHeight > $oldHeight) $newHeight = $oldHeight;
+						$ratioY = $newHeight / $oldHeight;
+
+					if ($ratioX < $ratioY) {
+						$startX = round(($oldWidth - ($newWidth / $ratioY))/2);
+						$startY = 0;
+						$oldWidth = round($newWidth / $ratioY);
+						$oldHeight = $oldHeight;
+					} else {
+						$startX = 0;
+						$startY = round(($oldHeight - ($newHeight / $ratioX))/2);
+						$oldWidth = $oldWidth;
+						$oldHeight = round($newHeight / $ratioX);
+					}
+					$applyWidth = $newWidth;
+					$applyHeight = $newHeight;
+					break;
+
+				case 'crop':
+					// -- a straight centered crop
+					$startY = ($oldHeight - $newHeight)/2;
+					$startX = ($oldWidth - $newWidth)/2;
+					$oldHeight = $newHeight;
+					$applyHeight = $newHeight;
+					$oldWidth = $newWidth;
+					$applyWidth = $newWidth;
+					break;
 				}
 
-				switch($ext)
-				{
-					case 'gif' :
-						$oldImage = imagecreatefromgif($srcimg);
-						break;
-					case 'png' :
-						$oldImage = imagecreatefrompng($srcimg);
-						break;
-					case 'jpg' :
-					case 'jpeg' :
-						$oldImage = imagecreatefromjpeg($srcimg);
-						break;
-					default :
-						//image type is not a possible option
-						return false;
-						break;
+				switch($ext) {
+				case 'gif' :
+					$oldImage = imagecreatefromgif($srcimg);
+					break;
+				case 'png' :
+					$oldImage = imagecreatefrompng($srcimg);
+					break;
+				case 'jpg' :
+				case 'jpeg' :
+					$oldImage = imagecreatefromjpeg($srcimg);
+					break;
+				default :
+					//image type is not a possible option
+					return false;
+					break;
 				}
 
 				//create new image
@@ -254,22 +242,21 @@ class ThumbnailComponent extends Object
 				//put old image on top of new image
 				imagecopyresampled($newImage, $oldImage, 0,0 , $startX, $startY, $applyWidth, $applyHeight, $oldWidth, $oldHeight);
 
-					switch($ext)
-					{
-						case 'gif' :
-							imagegif($newImage, $dstimg, $quality);
-							break;
-						case 'png' :
-							imagepng($newImage, $dstimg, $quality);
-							break;
-						case 'jpg' :
-						case 'jpeg' :
-							imagejpeg($newImage, $dstimg, $quality);
-							break;
-						default :
-							return false;
-							break;
-					}
+				switch($ext) {
+				case 'gif' :
+					imagegif($newImage, $dstimg, $quality);
+					break;
+				case 'png' :
+					imagepng($newImage, $dstimg, $quality);
+					break;
+				case 'jpg' :
+				case 'jpeg' :
+					imagejpeg($newImage, $dstimg, $quality);
+					break;
+				default :
+					return false;
+					break;
+				}
 
 				imagedestroy($newImage);
 				imagedestroy($oldImage);
@@ -280,16 +267,13 @@ class ThumbnailComponent extends Object
 		} else {
 			return false;
 		}
-
 	}
 
-	function image_type_to_extension($imagetype)
-	{
-	if(empty($imagetype)) return false;
-		switch($imagetype)
-		{
+	function image_type_to_extension($imagetype) {
+		if(empty($imagetype)) return false;
+		switch($imagetype) {
 			case IMAGETYPE_GIF    : return 'gif';
-			case IMAGETYPE_JPEG    : return 'jpg';
+			case IMAGETYPE_JPEG   : return 'jpg';
 			case IMAGETYPE_PNG    : return 'png';
 			case IMAGETYPE_SWF    : return 'swf';
 			case IMAGETYPE_PSD    : return 'psd';
@@ -302,10 +286,10 @@ class ThumbnailComponent extends Object
 			case IMAGETYPE_JB2    : return 'jb2';
 			case IMAGETYPE_SWC    : return 'swc';
 			case IMAGETYPE_IFF    : return 'aiff';
-			case IMAGETYPE_WBMP    : return 'wbmp';
+			case IMAGETYPE_WBMP   : return 'wbmp';
 			case IMAGETYPE_XBM    : return 'xbm';
-			default                : return false;
+			default               : return false;
 		}
 	}
-	} 
+}
 ?>
